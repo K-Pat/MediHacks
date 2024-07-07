0a170fd047f76535245604e50ce208cd63aed6ec
Kavyan Patel <kavyanpatel1104@gmail.com>
2024-07-07 16:25:04 -0700

    Added Middleware to handle CORS Policy with Default and Custom origins

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,23 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import {Interview} from "./models/interview.js";
 import interviewsRoute from "./routes/interviewsRoute.js";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+//middleware to handle CORS policy
+//Option 1: Allowing all origins with default of cors(*)
+app.use(cors());
+//Option 2: Allow custom origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', "POST", "PUT", "DELETE"],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 app.get('/', (request, response) =>{
     console.log(request);
