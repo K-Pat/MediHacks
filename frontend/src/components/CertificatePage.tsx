2b890ac5820009b7c1c0b142956eb1d49249e899
Prachi Heda <prachiheda@gmail.com>
2024-07-11 16:53:56 -0700

    certiicate nice

--- frontend/src/components/CertificatePage.tsx
+++ frontend/src/components/CertificatePage.tsx
@@ -1,9 +1,12 @@
 // src/components/CertificatePage.tsx
 
-import React, { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Box, Button, FormControl, FormLabel, Input, VStack, Heading, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Input, VStack, HStack, Heading, Text, useColorModeValue, Image} from '@chakra-ui/react';
 import html2canvas from 'html2canvas';
+import badgeImage from '../assets/badge.png'; // Ensure you have this image in the specified path
+import PrachiSignature from '../assets/PrachiSignature.png'; // Ensure you have this image in the specified path
+import KavyanSignature from '../assets/KavyanSignature.png'; // Ensure you have this image in the specified path
 
 const CertificatePage = () => {
   const location = useLocation();
@@ -41,7 +44,7 @@ const CertificatePage = () => {
       p={4}
       bg={useColorModeValue('gray.50', 'gray.800')}
     >
-      <VStack spacing={8} width="100%" maxWidth="md">
+      <VStack spacing={8} width="100%" maxWidth="4xl">
         <FormControl id="name">
           <FormLabel>Enter your name</FormLabel>
           <Input
@@ -60,15 +63,30 @@ const CertificatePage = () => {
           border="2px solid"
           borderColor={useColorModeValue('gray.200', 'gray.600')}
           width="100%"
+          maxWidth="800px"
           textAlign="center"
+          fontFamily="'Cinzel', serif"
         >
-          <Heading size="lg" mb={4}>Certificate of Completion</Heading>
-          <Text fontSize="lg" mb={4}>This certifies that</Text>
-          <Heading size="md" mb={4}>{name || 'Your Name'}</Heading>
-          <Text fontSize="lg" mb={4}>has successfully completed the</Text>
-          <Heading size="md" mb={4}>{interviewType}</Heading>
-          <Text fontSize="lg" mb={4}>track as an {role}</Text>
-          <Text fontSize="sm" mt={4}>Issued on: {issueDate}</Text>
+          <Heading size="lg" mb={4} fontFamily="'Cinzel', serif">Certificate of Completion</Heading>
+          <Text fontSize="lg" mb={4} fontFamily="'Cinzel', serif">This certifies that</Text>
+          <Heading size="md" mb={4} fontFamily="'Cinzel', serif">{name || 'Your Name'}</Heading>
+          <Text fontSize="lg" mb={4} fontFamily="'Cinzel', serif">has successfully completed the</Text>
+          <Heading size="md" mb={4} fontFamily="'Cinzel', serif">{interviewType}</Heading>
+          <Text fontSize="lg" mb={4} fontFamily="'Cinzel', serif">track as an {role}</Text>
+          <Image src={badgeImage} alt="Badge" boxSize="100px" mt={4} mb={4} mx="auto" />
+          <Text fontSize="sm" mt={4} fontFamily="'Cinzel', serif">Issued on: {issueDate}</Text>
+          <HStack justifyContent="space-around" mt={8}>
+            <VStack spacing={0}>
+              <Image src={PrachiSignature} alt="Signature 1" width="200px" height="120px" objectFit="contain" />
+              <Text fontFamily="'Cinzel', serif" mt={2}>Prachi Heda</Text>
+              <Text fontFamily="'Cinzel', serif">CO-FOUNDER</Text>
+            </VStack>
+            <VStack spacing={0}>
+              <Image src={KavyanSignature} alt="Signature 2" width="200px" height="120px" objectFit="contain" />
+              <Text fontFamily="'Cinzel', serif" mt={2}>Kavyan Patel</Text>
+              <Text fontFamily="'Cinzel', serif">CO-FOUNDER</Text>
+            </VStack>
+          </HStack>
         </Box>
         <Button colorScheme="teal" onClick={handleDownloadCertificate}>
           Download Certificate
