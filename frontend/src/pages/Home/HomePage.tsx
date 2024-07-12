421503e2c491611f82406549f426d91e4c5bcee3
Prachi Heda <prachiheda@gmail.com>
2024-07-11 19:50:33 -0700

    homepage

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,4 +1,3 @@
-
 import { Box, Button, Heading, Text, VStack, HStack } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import Spline from '@splinetool/react-spline';
@@ -7,33 +6,39 @@ const HomePage = () => {
   const navigate = useNavigate();
 
   return (
-    <Box position="relative" width="100vw" height="100vh" overflow="hidden">
-          <Spline scene="https://prod.spline.design/ojM1d1nVcPacEoyY/scene.splinecode" />
+    <Box position="relative" width="100vw" height="100vh" overflow="hidden" bg="#E7EFFF">
       <Box
         position="absolute"
         top="0"
         left="0"
-        width="100%"
+        width="50%"
         height="100%"
         display="flex"
         alignItems="center"
-        justifyContent="center"
-        textAlign="center"
+        justifyContent="flex-start"
+        textAlign="left"
         color="black"
-        pointerEvents="none" // Add this line to ensure the Spline remains interactive
+        pointerEvents="none"
+        p={8}
       >
-        <VStack spacing={8} pointerEvents="all"> // Add pointerEvents="all" to the VStack to make the elements inside it interactive
-          <Heading size="2xl">Welcome to Our Cool Platform</Heading>
-          <Text fontSize="xl">Sign up now to get started!</Text>
+        <VStack spacing={6} align="flex-start" pointerEvents="all" maxW="lg">
+          <Heading size="4xl">AccredMed</Heading>
+          <Text fontSize="xl" color="gray.600">
+            The premier platform for nursing credentialing. Earn certificates through professional interviews and uplevel your career.
+          </Text>
+          <Text fontSize="lg">Sign up now to get started!</Text>
           <HStack spacing={4}>
-            <Button colorScheme="red" onClick={() => navigate('/signup')}>
+            <Button colorScheme="red" size="lg" onClick={() => navigate('/signup')}>
               Sign Up
             </Button>
-            <Button colorScheme="red" onClick={() => navigate('/login')}>
+            <Button colorScheme="red" size="lg" onClick={() => navigate('/login')}>
               Login
             </Button>
           </HStack>
         </VStack>
+      </Box>
+      <Box position="absolute" top="0" right="0" width="70%" height="100%">
+        <Spline scene="https://prod.spline.design/ojM1d1nVcPacEoyY/scene.splinecode" style={{ width: '100%', height: '100vh' }} />
       </Box>
     </Box>
   );
