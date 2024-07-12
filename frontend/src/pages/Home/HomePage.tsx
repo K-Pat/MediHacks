8f4dc7c16b7bd6ac4991d3b37b27f6b00fc14527
Prachi Heda <prachiheda@gmail.com>
2024-07-12 09:52:32 -0700

    navbar

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Heading, Text, VStack, HStack } from '@chakra-ui/react';
+import { Box, Button, Heading, Text, VStack, HStack, Flex, Spacer } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import Spline from '@splinetool/react-spline';
 
@@ -7,22 +7,32 @@ const HomePage = () => {
 
   return (
     <Box position="relative" width="100vw" height="100vh" overflow="hidden" bg="#E7EFFF">
-      <Box
+      <Flex as="nav" width="100%" p={4} bg="white" boxShadow="sm" position="absolute" top="0" zIndex="10">
+        <Box fontWeight="bold" fontSize="xl" cursor="pointer" onClick={() => navigate('/')}>
+          AccredMed
+        </Box>
+        <Spacer />
+        <HStack spacing={8}>
+          <Button variant="link" onClick={() => navigate('/about-us')}>About Us</Button>
+          <Button variant="link" onClick={() => navigate('/explore-tracks')}>Explore Tracks</Button>
+          <Button variant="link" onClick={() => navigate('/coming-soon')}>Coming Soon</Button>
+        </HStack>
+      </Flex>
+      
+      <Flex
         position="absolute"
         top="0"
         left="0"
-        width="50%"
+        width="100%"
         height="100%"
-        display="flex"
         alignItems="center"
-        justifyContent="flex-start"
-        textAlign="left"
+        justifyContent="space-around"
         color="black"
         pointerEvents="none"
         p={8}
       >
         <VStack spacing={6} align="flex-start" pointerEvents="all" maxW="lg">
-          <Heading size="4xl">AccredMed</Heading>
+          <Heading size="4xl">The New Standard in Nurse Upleveling</Heading>
           <Text fontSize="xl" color="gray.600">
             The premier platform for nursing credentialing. Earn certificates through professional interviews and uplevel your career.
           </Text>
@@ -31,15 +41,13 @@ const HomePage = () => {
             <Button colorScheme="red" size="lg" onClick={() => navigate('/signup')}>
               Sign Up
             </Button>
-            <Button colorScheme="red" size="lg" onClick={() => navigate('/login')}>
-              Login
-            </Button>
           </HStack>
         </VStack>
-      </Box>
-      <Box position="absolute" top="0" right="0" width="70%" height="100%">
-        <Spline scene="https://prod.spline.design/ojM1d1nVcPacEoyY/scene.splinecode" style={{ width: '100%', height: '100vh' }} />
-      </Box>
+        
+        <Box width="60%" height="100%">
+          <Spline scene="https://prod.spline.design/ojM1d1nVcPacEoyY/scene.splinecode" style={{ width: '100%', height: '100vh' }} />
+        </Box>
+      </Flex>
     </Box>
   );
 };
